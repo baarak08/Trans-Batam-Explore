2e2a7729ed80a6d08843ba97a48380a4493b585f
M N Ikhsan Mubaarak <126537753+baarak08@users.noreply.github.com>
2024-03-13 13:33:55 +0700

    Initial Commit

---
+++ Trans Batam Explore/Trans_Batam_ExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trans_Batam_ExploreApp.swift
+//  Trans Batam Explore
+//
+//  Created by Muhammad Nur Ikhsan Mubaarak on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Trans_Batam_ExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
